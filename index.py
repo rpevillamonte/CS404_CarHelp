fc57495455ed8e60ca7c6f8464e6e8a2bf2ade0b
kim-anc <qkbanchuvas@tip.edu.ph>
2021-06-17 23:43:12 +0800

    Added light icons

--- index.py
+++ index.py
@@ -18,16 +18,26 @@ def about_us():
 
 @app.route('/carhelp')
 def car_help():
-    lights = ['Oil Pressure Light', 'Engine Temperature Light',
-              'ABS Light', 'Handbrake/Parking Brake Light', 'Traction Control Light',
-              'Engine Warning Light', 'Battery Alert Light', 'Tire Pressure Light',
-              'Fuel Indicator', 'Air Bag Indicator', 'Seat Belt Indicator', 'Washer Fluid Indicator']
+    lights = [['Oil Pressure Light', '/img/Icons/Oil_Pressure_Light.png'],
+              ['Engine Temperature Light', '/img/Icons/Engine_Temperature_Light.png'],
+              ['ABS Light', '/img/Icons/ABS_Light.png'],
+              ['Handbrake/Parking Brake Light', '/img/Icons/Handbrake_Light.png'],
+              ['Traction Control Light', '/img/Icons/Traction_Control_Light.png'],
+              ['Engine Warning Light', '/img/Icons/Engine_Warning_Light.png'],
+              ['Battery Alert Light', '/img/Icons/Battery_Alert_Light.png'],
+              ['Tire Pressure Light', '/img/Icons/Tire_Pressure_Light.png'],
+              ['Fuel Indicator', '/img/Icons/Fuel_Indicator_Light.png'],
+              ['Air Bag Indicator', '/img/Icons/Airbag_Indicator_Light.png'],
+              ['Seat Belt Indicator', '/img/Icons/Seatbelt_Warning_Light.png'],
+              ['Washer Fluid Indicator', '/img/Icons/Washer_Fluid_Indicator.png']
+             ]
     problems = ['Knocking on the engine', 'Steering wheel is shaking when at use', 'Squeaking or grinding noise when braking',
                 'Steering feels heavy', 'Car does not accelerate, but RPM goes up', 'Car is consuming too much oil',
                 'Battery is not charging (even if it is a newly replaced battery)', 'Radiator leaking fluid',
                 'Turning the key and nothing happens', 'Too much smoke from the exhaust pipe', 'When changing gear, it will not engage easily / it will not stay in gear (Note: Applies to Manual Transmission)',
                 'Fluid is leaking in the differential part', 'Grinding noise when steering and difficulty in steering',
                 'Dropping gas mileage and loss of power']
+
     return render_template('carhelp.html', lights=lights, problems=problems)
 
 #problem-choice: petrol or diesel
